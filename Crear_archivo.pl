d833d73d917f514a13fada438ca5e0fdfc2327a5
TheAlexBig <00062816@uca.edu.sv>
2018-10-15 07:34:13 -0600

    Actualizacion de ejercicios

---
+++ Crear_archivo.pl
@@ -0,0 +1,42 @@
+crearArchivo:-
+    open('Generados/Primero.txt',write,Archivo),
+    close(Archivo).
+
+leerArchivo:-
+    open('Generados/Primero.txt',read,Archivo),
+    read(Archivo,Variable),
+    writeln(Variable),
+    close(Archivo).
+
+
+escribir_nombre:-
+    open('Generados/Primero.txt',write,Flujo),
+    write('Escriba su nombre: '),
+    read(Nombre),
+    write(Flujo,Nombre),
+    close(Flujo).
+
+
+lectura_repetida:-
+    open('Generados/Numeros.txt',read,Flujo),
+    ciclo(Flujo),
+    close(Flujo).
+
+
+ciclo(Flujo):-
+    read(Flujo,Aux),
+    \==(Aux, end_of_file),
+    write(Aux),
+    write('\n'),
+    ciclo(Flujo).
+
+ciclo(Flujo):-
+    read(Flujo,Aux),
+    ==(Aux, end_of_file).
+
+
+
+
+
+
+
